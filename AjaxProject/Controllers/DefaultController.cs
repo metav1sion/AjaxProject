4754c605495e420dbb1707ea18f528edef84893c
Metehan Sözen <metehansozen@outlook.com>
2024-06-26 13:20:11 +0300

    Add project files.

---
+++ AjaxProject/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+using AjaxProject.Dal;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace AjaxProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductList()
+        {
+            var values = _context.Products.ToList();
+            var jsonValues = JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            var newproduct = JsonConvert.SerializeObject(product);
+            return Json(newproduct);
+        }
+    }
+}
---
+++ AjaxProject/Dal/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.Dal
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= DESKTOP-JTNUQFE\\SQLEXPRESS ;initial Catalog=AjaxDb; integrated Security=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
