2f82ec3d984451c207947fcba767c125636f653a
Metehan Sözen <metehansozen@outlook.com>
2024-06-28 18:45:07 +0300

    Ajax ile Ürün Listeleme, Ekleme, Silme, Güncelleme ve ID'ye göre getirme işlemleri yapıldı.

--- AjaxProject/Controllers/DefaultController.cs
+++ AjaxProject/Controllers/DefaultController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json.Serialization;
 using AjaxProject.Dal;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
 namespace AjaxProject.Controllers
@@ -33,6 +34,34 @@ namespace AjaxProject.Controllers
             _context.SaveChanges();
             var newproduct = JsonConvert.SerializeObject(product);
             return Json(newproduct);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x=>x.ProductId == id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            var productResponse = JsonConvert.SerializeObject(product);
+            //return Json(productResponse);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public  IActionResult UpdateProduct(Product product)
+        {
+            //var product = _context.Products.FirstOrDefault(x=>x.ProductId == id);
+            _context.Products.Update(product);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        
+        public IActionResult GetProduct(int id)
+        {
+            var value = _context.Products.FirstOrDefault(x=>x.ProductId == id);
+            var jsonValue = JsonConvert.SerializeObject(value);
+            return Json(jsonValue);
         }
     }
 }
